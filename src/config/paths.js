54b3c90bdd9dd0367050579b3e0bbb5bbca095be
unknown <charleselie.piquemal@gmail.com>
2020-11-16 23:00:00 +0100

    auto route

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,2 +1,3 @@
 export const VIEW_HOME_PATH = "/";
 export const VIEW_AUTH_PATH = "/login";
+export const VIEW_ADMIN_PATH = "/admin";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Home from './Home/Home';
 import Authentification from './Authentification/Authentification';
+import Error404 from './404/404';
 
 export default {
     Home,
-    Authentification
+    Authentification,
+    Error404
 }
